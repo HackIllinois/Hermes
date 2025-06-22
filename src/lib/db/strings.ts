2a560bd25a2f9ec25e845f97289eeea005fe83c9
akulsharma1 <akulsharma02@gmail.com>
2025-06-21 22:22:19 -0700

    adjusted table logic and added const for table names

---
+++ src/lib/db/strings.ts
@@ -0,0 +1,7 @@
+export const Tables = {
+    CONTACT_TASKS: "contact_tasks",
+    EMAILS: "emails",
+    EMAIL_THREADS: "email_threads",
+    PROFILES: "profiles",
+    SPONSORS: "sponsors",
+} as const;
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,4 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 import { config } from "../config";
 import { Database } from "./db/schemas";
 
-export const supabase = createClient<Database>(config.SUPABASE_URL, config.SUPABASE_KEY);
+export const supabase = createClient<Database>(config.SUPABASE_URL, config.SUPABASE_KEY, { auth: { flowType: "pkce" } });
