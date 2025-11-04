bb90a40bdfd45e769ac521f411812f0b35d5aec5
akulsharma1 <akulsharma02@gmail.com>
2025-11-04 12:40:07 -0600

    added default contact email as env variable

--- src/config.ts
+++ src/config.ts
@@ -9,12 +9,20 @@ export const config = {
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
     ENVIRONMENT: process.env.ENVIRONMENT || "DEV",
     GMAIL_PUBSUB_TOPIC: process.env.GMAIL_PUBSUB_TOPIC || "",
+    DEFAULT_CONTACT_EMAIL: process.env.DEFAULT_CONTACT_EMAIL || "",
 } as const;
 
 export const isProductionEnvironment = config.ENVIRONMENT === "PROD";
 
 export const validateEnv = (): void => {
-    const requiredEnvVars = ["SUPABASE_URL", "SUPABASE_KEY", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "GMAIL_PUBSUB_TOPIC"];
+    const requiredEnvVars = [
+        "SUPABASE_URL",
+        "SUPABASE_KEY",
+        "GOOGLE_CLIENT_ID",
+        "GOOGLE_CLIENT_SECRET",
+        "GMAIL_PUBSUB_TOPIC",
+        "DEFAULT_CONTACT_EMAIL",
+    ];
 
     for (const envVar of requiredEnvVars) {
         if (!process.env[envVar]) {
@@ -24,4 +32,5 @@ export const validateEnv = (): void => {
 };
 
 export const BASE_FRONTEND_URL = config.ENVIRONMENT == "DEV" ? "http://localhost:3000" : "https://hermes.hackillinois.org";
-export const BASE_BACKEND_URL = config.ENVIRONMENT == "DEV" ? `http://localhost:5555/api` : "https://hermes-api.hackillinois.org/api";
+export const BASE_BACKEND_URL =
+    config.ENVIRONMENT == "DEV" ? `http://localhost:5555/api` : "https://hermes-api.hackillinois.org/api";
