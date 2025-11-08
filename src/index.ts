90b814680fbf3f368a67d9d9c1cd61c7ea7cb78a
akulsharma1 <akulsharma02@gmail.com>
2025-11-08 16:32:31 -0600

    created scheduled send logic and cron job

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import app from "./app";
 import cron from "node-cron";
 import { renewAllGmailPubSubWatchers } from "./services/pubsub/pubsub-cron";
 import { config } from "./config";
+import { processScheduledSends } from "./services/emails/email-cron";
 
 const PORT = process.env.PORT || 5555;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} in ${config.ENVIRONMENT} environment`);
 
-    console.log("Scheduling daily Gmail watch renewal (at 6:00 AM UTC).");
+    console.log("Scheduling cron job for daily Gmail watch renewal (at 6:00 AM UTC).");
     cron.schedule(
         "0 6 * * *",
         () => {
@@ -17,6 +18,18 @@ app.listen(PORT, () => {
         },
         {
             timezone: "UTC", // Explicitly set timezone for predictability
+        },
+    );
+
+    console.log("Scheduling cron job for process scheduled sends (every minute).");
+    cron.schedule(
+        "* * * * *", // Runs every minute
+        () => {
+            // The function itself logs its own start/end
+            processScheduledSends();
+        },
+        {
+            // No timezone needed for "every minute"
         },
     );
 });
